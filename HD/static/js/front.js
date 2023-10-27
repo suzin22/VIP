c7e328a842e6fe680d3af4c638aad831a2a6fe3a
VW <89129570+V-Won@users.noreply.github.com>
2023-10-27 11:24:09 +0900

    counter 수정 건.

--- HD/static/js/front.js
+++ HD/static/js/front.js
@@ -142,7 +142,16 @@ $(document).ready(function(){
   }
   // HD현대 in social swiper 
 
-  // main 주가정보 s
+  // main 주가정보
+  const counterHtml = document.querySelector('.counter').innerHTML
+
+  window.addEventListener('scroll',(e) => {
+    const mainSocialOffsetTop = document.querySelector('.main-social').offsetTop
+    if( window.scrollY + 500 <= mainSocialOffsetTop){
+      document.querySelector('.counter').innerHTML = counterHtml
+    }
+  })
+  
   $('.counter').counterUp({
     delay: 10,
     time: 1000,
