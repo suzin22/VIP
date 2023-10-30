6df75f816aa811957bd41bde774e382e561d56f5
suhyunzzang <suzzang@v-wonderland.com>
2023-10-30 14:00:26 +0900

    ㅇ

---
+++ HD/static/js/front_mob.js
@@ -0,0 +1,34 @@
+$(function(){
+  // HD현대 in social swiper 
+  function mainSocialSwiper() {
+    var social_swiper = new Swiper(".main-social .social-swiper", {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      pagination: {
+        el: ".social-progress-bar .swiper-pagination",
+        type: "progressbar",
+      },
+    })
+  }
+  // HD현대 in social swiper 
+
+  // main 주가정보
+  const counterHtml = document.querySelector('.counter').innerHTML
+
+  window.addEventListener('scroll',(e) => {
+    const mainSocialOffsetTop = document.querySelector('.main-social').offsetTop
+    if( window.scrollY + 500 <= mainSocialOffsetTop){
+      document.querySelector('.counter').innerHTML = counterHtml
+    }
+  })
+  
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+  });
+  // main 주가정보 e
+
+  mainSocialSwiper();
+
+
+})
