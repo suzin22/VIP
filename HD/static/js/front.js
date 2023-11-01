b00a86e2723910f35be653efc1dc3774bf808fdb
VW <89129570+V-Won@users.noreply.github.com>
2023-11-01 16:34:23 +0900

    e

--- HD/static/js/front.js
+++ HD/static/js/front.js
@@ -446,3 +446,17 @@ $(document).ready(function(){
     }
   });
 })
+
+// 스크롤 스무스
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
