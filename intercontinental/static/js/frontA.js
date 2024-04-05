aefefd63986d1651578685f2bfcf50752bbe4264
VW <89129570+V-Won@users.noreply.github.com>
2024-04-05 16:18:13 +0900

    .

--- intercontinental/static/js/frontA.js
+++ intercontinental/static/js/frontA.js
@@ -1,44 +1,44 @@
 setTimeout(() => {
   // 마우스커서
-  gsap.set(".window-cursor", { xPercent: -50, yPercent: -50 });
+  // gsap.set(".window-cursor.type-1", { xPercent: -50, yPercent: -50 });
 
-  const Tc = document.querySelector(".window-cursor"),
-    Pc = document.querySelectorAll("a, button");
+  // const Tc3 = document.querySelector(".window-cursor.type-1"),
+  //   Pc3 = document.querySelectorAll("a, button");
 
-  let xTo = gsap.quickTo(".window-cursor", "x", { duration: 0.6, ease: "power3" }),
-    yTo = gsap.quickTo(".window-cursor", "y", { duration: 0.6, ease: "power3" });
+  // let xTo3 = gsap.quickTo(".window-cursor.type-1", "x", { duration: 0.6, ease: "power3" }),
+  //   yTo3 = gsap.quickTo(".window-cursor.type-1", "y", { duration: 0.6, ease: "power3" });
 
-  window.addEventListener("mousemove", e => {
-    xTo(e.clientX);
-    yTo(e.clientY);
-  });
+  // window.addEventListener("mousemove", e => {
+  //   xTo3(e.clientX);
+  //   yTo3(e.clientY);
+  // });
 
-  document.addEventListener("mouseenter", (function () {
-    Tc.style.opacity = 1
-  }
-  ));
-  document.addEventListener("mouseleave", (function () {
-    Tc.style.opacity = 0
-  }
-  ));
+  // document.addEventListener("mouseenter", (function () {
+  //   Tc3.style.opacity = 1
+  // }
+  // ));
+  // document.addEventListener("mouseleave", (function () {
+  //   Tc3.style.opacity = 0
+  // }
+  // ));
 
-  // main swiper-button hover
-  let swiperPrev = document.querySelector('.box-main-kv .swiper-button-prev'),
-    swiperNext = document.querySelector('.box-main-kv .swiper-button-next');
+  // // main swiper-button hover
+  // let swiperPrev = document.querySelector('.box-main-kv .swiper-button-prev'),
+  //   swiperNext = document.querySelector('.box-main-kv .swiper-button-next');
 
-  if (document.querySelectorAll('.box-main-kv').length) {
-    swiperPrev.addEventListener('mouseover', () => {
-      Tc.classList.add('main-swiperButton-prev');
-    })
-    swiperPrev.addEventListener('mouseleave', () => {
-      Tc.classList.remove('main-swiperButton-prev');
-    })
-    swiperNext.addEventListener('mouseover', () => {
-      Tc.classList.add('main-swiperButton-next');
-    })
-    swiperNext.addEventListener('mouseleave', () => {
-      Tc.classList.remove('main-swiperButton-next');
-    })
-  }
+  // if (document.querySelectorAll('.box-main-kv').length) {
+  //   swiperPrev.addEventListener('mouseover', () => {
+  //     Tc3.classList.add('main-swiperButton-prev');
+  //   })
+  //   swiperPrev.addEventListener('mouseleave', () => {
+  //     Tc3.classList.remove('main-swiperButton-prev');
+  //   })
+  //   swiperNext.addEventListener('mouseover', () => {
+  //     Tc3.classList.add('main-swiperButton-next');
+  //   })
+  //   swiperNext.addEventListener('mouseleave', () => {
+  //     Tc3.classList.remove('main-swiperButton-next');
+  //   })
+  // }
   // //마우스커서
 }, 500);
